4b30ffb5e866898712a414744dc78d0a672c0ea5
Andre Gustavo <andre.contas125@gmail.com>
2022-04-25 19:52:18 -0300

    new code

--- Loading/main.cpp
+++ Loading/main.cpp
@@ -1,33 +1,60 @@
 #include <iostream>
-#include <Windows.h>
 
-void loading()
-{
-	char progress = 219;
+using namespace std;
 
-	std::cout.width(40);
-	std::cout << "Loading\n";
-	std::cout.width(20);
+char process = 219;
+char space = ' ';
 
-	for (int i = 0; i < 35; i++)
-	{
-		std::cout << progress;
+// Function Prototype
+void Delay(int x);
+void LoadingType1();
+void LoadingType2();
 
-		if (i < 10)
-			Sleep(300);
-		if (i >= 10 && i < 20)
-			Sleep(150);
-		if (i >= 10)
-			Sleep(25);
-	}
-	std::cout << std::endl;
-	std::cout.width(42);
-	std::cout << "COMPLETE!\n";
-}
-
+// Functions
 int main()
 {
-	loading();
+	cout << endl;
+	cout.width(40);
+	cout << "loading style 1" << endl;
+	LoadingType1();
+
+	cout << endl << endl;
+	cout.width(40);
+	cout << "loading style 2" << endl;
+	LoadingType2();
+
 	return 0;
 }
+
+void Delay(int x)
+{
+	for (int i = 0; i <= x; i++)
+	{
+		for (int z = 0; z <= x; z++)
+		{
+			//loop
+		}
+	}
+}
+
+void LoadingType1()
+{
+	cout.width(20);
+	for (int i = 0; i < 25; i++)
+	{
+		Delay(9500);
+		cout << process;
+	}
+}
+
+void LoadingType2()
+{
+	cout.width(20);
+	for (int i = 0; i < 13; i++)
+	{
+		Delay(9500);
+		cout << process << space;
+	}
+}
+
 
